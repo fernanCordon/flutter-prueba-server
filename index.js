29110eaf14d1cdc78f1171bbaab042211181de1a
Fernando Cordón <fcordon@pin.upv.es>
2020-12-29 00:24:08 +0100

    5º commit

--- index.js
+++ index.js
@@ -5,28 +5,12 @@ require('dotenv').config();
 const app = express();
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
-// Mensajes de Sockets
+// Cambio la const por un módulo para exportarlo
+module.exports.io = require('socket.io')(server);
 
-io.on('connection', client => {
-    console.log('Cliente conectado');
-
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
-     });
-
-    // Voy a estar escuchando un 'mensaje' y detrás haré algo
-    // en payload va el objeto que envian
-    client.on('mensaje', ( payload ) => {
-        console.log('Mensaje', payload);
-
-        // El servidor va a emitir un mensaje a todos los clientes conectados
-        io.emit('mensaje', { admin: 'Nuevo mensaje'});
-
-    });
-});
-
+// Llamo al archivo donde he movido la comunicación por sockets
+require('./sockets/socket');
 
 const publicPath = path.resolve( __dirname, 'public' )
 app.use( express.static( publicPath ) );
