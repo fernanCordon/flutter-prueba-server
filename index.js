6724f0a807d6f33f3ad618146d91467c64ba3a17
Fernando Cordón <fcordon@pin.upv.es>
2020-12-28 23:39:33 +0100

    3º commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const path = require('path');
-
-// para el paquete dotenv que he descargado (npm i dotenv)
-// Cuando esté en producción tendré que escuchar el puerto que haya en el dominio
-// Leerá el archivo .env y establecerá las variables de entorno
 require('dotenv').config();
 
+// He puesto la app de Express encima del server
 const app = express();
 
-const publicPath = path.resolve( __dirname, 'public' )
+// Node Server a la que le asigno el app de express
+const server = require('http').createServer(app);
+// Configuración propia del server de socket: io (in y out: info que viene y va)
+// Le mandamos de argumento el servidor Node que hemos creado
+const io = require('socket.io')(server);
 
+// Mensajes de Sockets
+
+// El client es una computadora o dispositivo que se acaba de conectar a mi Socket server
+// Cuando la web se recargue, esa conexión de ese navegador web cae a client
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+     })
+});
+
+
+const publicPath = path.resolve( __dirname, 'public' )
 app.use( express.static( publicPath ) );
 
-// Y así puedo leer el puerto desde la variable de entorno
-app.listen(process.env.PORT, (err) => {
+// Cambiamos app por server
+server.listen(process.env.PORT, (err) => {
     if ( err ) throw new Error(err);
-    // para que me saque el puerto en el que está corriendo
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
