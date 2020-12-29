14f5f260daa0a233e6efc144c680eba1dc560940
Fernando Cordón <fcordon@pin.upv.es>
2020-12-29 22:03:16 +0100

    B - 6º commit

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const app = express();
 
 const server = require('http').createServer(app);
 
-// Cambio la const por un módulo para exportarlo
 module.exports.io = require('socket.io')(server);
 
-// Llamo al archivo donde he movido la comunicación por sockets
 require('./sockets/socket');
 
 const publicPath = path.resolve( __dirname, 'public' )
@@ -19,3 +17,4 @@ server.listen(process.env.PORT, (err) => {
     if ( err ) throw new Error(err);
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
+
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,5 +1,4 @@
 
-// Traigo io
 const { io } = require('../index');
 
 // Mensajes de Sockets
@@ -12,8 +11,28 @@ io.on('connection', client => {
 
     client.on('mensaje', ( payload ) => {
         console.log('Mensaje', payload);
-
         io.emit('mensaje', { admin: 'Nuevo mensaje'});
-
     });
+
+    // Escucho cuando un cliente emita ese mensaje
+    client.on('emitir-mensaje', ( payload ) => {
+
+        // Después de emitir el HEY!!! ABRO CHROME EN localhost:3000 y en la consola pongo:
+        // socket.emit('emitir-mensaje', 'Fernando');
+        // Presiono ENTER y en la debug de Flutter cayó el mensaje HEY!!! ¿Porqué?
+        // Porque esto me llegó aquí al servidor y yo envié 'nuevo-mensaje' que lleva el HEY!!!
+        // Así que como Flutter está escuchando 'nuevo-mensaje' llegó hasta allí
+
+        // io.emit('nuevo-mensaje', 'HEY!!!!!');
+
+        // io.emit('nuevo-mensaje', payload); // EMITE A TODOS LOS CLIENTES CONECTADOS
+
+        // Si quiero emitir a todos menos al cliente que lo emitió (en este caso el navegador, o el que sea)
+        // Si yo habla, el servidor no me repetirá lo que he dicho
+        client.broadcast.emit('nuevo-mensaje', payload);
+
+        //VUELVO AL NAVEGADOR A EMITIR ESTE MENSAJE
+        // socket.emit('emitir-mensaje', {nombre: 'Fernando', mensaje: 'Hola Mundo'});
+    });
+
 });
