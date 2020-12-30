4cc9598f9657a0d35519c1190de67b2cd66530cf
Fernando Cordón <fcordon@pin.upv.es>
2020-12-30 10:33:38 +0100

    C - 7º commit

--- sockets/socket.js
+++ sockets/socket.js
@@ -14,25 +14,15 @@ io.on('connection', client => {
         io.emit('mensaje', { admin: 'Nuevo mensaje'});
     });
 
-    // Escucho cuando un cliente emita ese mensaje
     client.on('emitir-mensaje', ( payload ) => {
-
-        // Después de emitir el HEY!!! ABRO CHROME EN localhost:3000 y en la consola pongo:
-        // socket.emit('emitir-mensaje', 'Fernando');
-        // Presiono ENTER y en la debug de Flutter cayó el mensaje HEY!!! ¿Porqué?
-        // Porque esto me llegó aquí al servidor y yo envié 'nuevo-mensaje' que lleva el HEY!!!
-        // Así que como Flutter está escuchando 'nuevo-mensaje' llegó hasta allí
-
-        // io.emit('nuevo-mensaje', 'HEY!!!!!');
-
-        // io.emit('nuevo-mensaje', payload); // EMITE A TODOS LOS CLIENTES CONECTADOS
-
-        // Si quiero emitir a todos menos al cliente que lo emitió (en este caso el navegador, o el que sea)
-        // Si yo habla, el servidor no me repetirá lo que he dicho
+        
+        // Me aseguro de que recibo el mensaje en el servidor, así que debería verse 
+        // algo en la consola de la terminal del servidor
+        // console.log(payload);
+        
+        // Primero desactivé esto cuando puse el console de arriba y cuando he visto que
+        // funciona lo activo
         client.broadcast.emit('nuevo-mensaje', payload);
-
-        //VUELVO AL NAVEGADOR A EMITIR ESTE MENSAJE
-        // socket.emit('emitir-mensaje', {nombre: 'Fernando', mensaje: 'Hola Mundo'});
     });
 
 });
